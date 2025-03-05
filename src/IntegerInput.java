2d692d23646e06de2595e3b8b1440c7b5001b255
Rakshith272003 <rakshithmdpura@gmail.com>
2025-03-05 14:04:45 +0530

    First

---
+++ src/IntegerInput.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Scanner;
+
+
+public class IntegerInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n");
+        int n= sc.nextInt();
+        System.out.println("Entered integer is:"+n);
+        sc.close();
+    }
+}
